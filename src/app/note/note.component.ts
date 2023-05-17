8991a2b1c1305669ec4c69876fa26378f2568587
BLUECOMET <mmastrangelo1120@gmail.com>
2023-05-17 10:05:06 -0400

    created all content

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -9,22 +9,27 @@ export class NoteComponent {
   staticData = [
     {
       title: "Schedule 1",
+      category: "Entertainment",
       content: "Go to the cinema."
     },
     {
       title: "Schedule 2",
+      category: "Entertainment",
       content: "Play football"
     },
     {
       title: "Schedule 3",
+      category: "Entertainment",
       content: "Join the competition."
     },
     {
       title: "Schedule 4",
+      category: "Meal",
       content: "Have a breakfast and go to bed"
     },
     {
       title: "Schedule 5",
+      category: "Meal",
       content: "Get up and have a dinner."
     }
   ]
